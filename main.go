f945e4e233340ed0965c570cbbb5ab1782687ff7
pixel <pixel+github@chrissx.de>
2025-05-04 16:33:35 +0200

    prometheus metrics

--- main.go
+++ main.go
@@ -5,11 +5,18 @@ import (
 	"net/http"
 
 	"github.com/chrissxMedia/cm3.go"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 func main() {
-	// TODO: prometheus
+	var reqs = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "erwin_requests",
+		Help: "Number of HTTP requests by IP.",
+	}, []string{"ip"})
+	cm3.HandleMetrics(reqs)
 	cm3.ListenAndServeHttp(":8080", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, cm3.RemoteIp(r))
+		ip := cm3.RemoteIp(r)
+		reqs.WithLabelValues(ip).Inc()
+		fmt.Fprintln(w, ip)
 	})
 }
