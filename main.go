fe2b5ffef967f844faa05738c1c73e7480037e1c
pixel <pixel+github@chrissx.de>
2025-09-29 04:23:36 +0200

    work around the `[ip]` issue

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/chrissxMedia/cm3.go"
 	"github.com/prometheus/client_golang/prometheus"
@@ -16,6 +17,8 @@ func main() {
 	cm3.HandleMetrics(reqs)
 	cm3.ListenAndServeHttp(":8080", func(w http.ResponseWriter, r *http.Request) {
 		ip := cm3.RemoteIp(r)
+		// TODO: break cm3.RemoteIp
+		ip = strings.ReplaceAll(strings.ReplaceAll(ip, "[", ""), "]", "")
 		reqs.WithLabelValues(ip).Inc()
 		fmt.Fprintln(w, ip)
 	})
